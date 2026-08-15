a42528117e47ad48a2d33f3b98ad5daa0d69fd33
Mtzallqmy <2.08401685e+08+Mtzallqmy@users.noreply.github.com>
2026-08-15 16:15:26 +0000

    fix: enable serialization plugin for OpenAI provider

--- provider/openai/build.gradle.kts
+++ provider/openai/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library") version "8.5.2"
     id("org.jetbrains.kotlin.android") version "1.9.24"
-    
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
 }
 
 android {
